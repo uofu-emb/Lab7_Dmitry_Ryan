a9692b1cae0ef30c61a35f05a6b246cb5431706d
Ryan Ma <lunaticasian1@gmail.com>
2023-10-23 17:00:25 -0600

    midway through activity 0

--- src/main.c
+++ src/main.c
@@ -7,7 +7,29 @@
 #include <sys/byteorder.h>
 
 
-const struct device *can_dev;
 
-can_dev = device_get_binding("CAN_1");
-can_set_mode(can_dev, CAN_LOOPBACK_MODE);
+
+
+
+
+void main(void)
+{
+   //Activity 0
+struct zcan_frame frame = {
+                .id_type = CAN_STANDARD_IDENTIFIER,
+                .rtr = CAN_DATAFRAME,
+                .id = 0x123,
+                .dlc = 8,
+                .data = {1,2,3,4,5,6,7,8}
+        };
+        const struct device *can_dev;
+        int ret;
+
+        can_dev = device_get_binding("CAN_1");
+        can_set_mode(can_dev, CAN_LOOPBACK_MODE);
+
+        ret = can_send(can_dev, &frame, K_MSEC(100), NULL, NULL);
+        if (ret != CAN_TX_OK) {
+                printk("Sending failed [%d]", ret);
+        }     
+}
