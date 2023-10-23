a93676069d932b673aeee349bbbf49c2ad717e44
Ryan Ma <lunaticasian1@gmail.com>
2023-10-23 17:36:02 -0600

    Completed activity 0

--- src/main.c
+++ src/main.c
@@ -6,30 +6,61 @@
 #include <drivers/gpio.h>
 #include <sys/byteorder.h>
 
-
-
-
-
-
-
-void main(void)
-{
-   //Activity 0
+//messsage
 struct zcan_frame frame = {
                 .id_type = CAN_STANDARD_IDENTIFIER,
                 .rtr = CAN_DATAFRAME,
                 .id = 0x123,
                 .dlc = 8,
                 .data = {1,2,3,4,5,6,7,8}
+
         };
-        const struct device *can_dev;
-        int ret;
 
-        can_dev = device_get_binding("CAN_1");
-        can_set_mode(can_dev, CAN_LOOPBACK_MODE);
+//filter
+const struct zcan_filter my_filter = {
+        .id_type = CAN_STANDARD_IDENTIFIER,
+        .rtr = CAN_DATAFRAME,
+        .id = 0x123,
+        .rtr_mask = 1,
+        .id_mask = CAN_STD_ID_MASK
+};
 
+const struct device *can_dev;
+int ret;
+int filter_id1;
+
+void send_message(void)
+{
         ret = can_send(can_dev, &frame, K_MSEC(100), NULL, NULL);
         if (ret != CAN_TX_OK) {
-                printk("Sending failed [%d]", ret);
-        }     
+                        printk("Sending failed [%d]", ret);
+                }     
+}
+
+
+
+void rx_callback_function(struct zcan_frame *frame, void *arg)
+{
+        filter_id1 = can_attach_isr(can_dev, rx_callback_function, arg, &my_filter);
+        if (filter_id1 < 0) {
+                printk("Unable to attach isr [%d]", filter_id1);
+        }
+}
+
+void main(void)
+{
+   //Activity 0
+        can_dev = device_get_binding("CAN_1");
+        can_set_mode(can_dev, CAN_LOOPBACK_MODE);
+        
+        while(1)
+        {
+                send_message();
+        }
+        
+        rx_callback_function(&frame, NULL);
+
+        
+        //printk("Print data[%d]", can_dev->data);
+        
 }
